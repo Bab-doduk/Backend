b2c37e6600fb45343731f714d19ace9445d1c4c4
lsw71311 <lsw71311@naver.com>
2024-08-23 16:06:08 +0900

    Init commit

---
+++ src/test/java/com/sparta/bobdoduk/BobDodukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bobdoduk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobDodukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
