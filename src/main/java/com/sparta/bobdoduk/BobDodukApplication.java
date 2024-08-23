a8581c30fb693279d56fc15e00b635ed6a666415
kyungjinleelee <lv_jin@naver.com>
2024-08-23 14:55:22 +0900

    init first commit

---
+++ src/main/java/com/sparta/bobdoduk/BobDodukApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bobdoduk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobDodukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobDodukApplication.class, args);
+    }
+
+}
