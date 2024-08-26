be7abd6925a1a3ab60aed98d73ceb37e5d694e2d
kyungjinleelee <lv_jin@naver.com>
2024-08-26 17:05:11 +0900

    feat #4: BaseEntity 생성

--- src/main/java/com/sparta/bobdoduk/BobDodukApplication.java
+++ src/main/java/com/sparta/bobdoduk/BobDodukApplication.java
@@ -2,8 +2,10 @@ package com.sparta.bobdoduk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BobDodukApplication {
 
     public static void main(String[] args) {
