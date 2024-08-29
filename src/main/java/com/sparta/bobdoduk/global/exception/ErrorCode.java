8a04db7482f4e5a63c585b19aa0bfe47e7be1215
kyungjinleelee <lv_jin@naver.com>
2024-08-29 19:29:23 +0900

    feat #4: 접근 불가 (권한 없음) 에러 추가

--- src/main/java/com/sparta/bobdoduk/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/bobdoduk/global/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     AREA_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 지역 카테고리가 존재하지 않습니다."),
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "리뷰를 찾을 수 없습니다."),
     PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "결제 내역을 찾을 수 없습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
     // ------ 5xx ------
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다."),
     ;
