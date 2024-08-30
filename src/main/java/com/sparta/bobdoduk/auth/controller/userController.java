60dad024a5cb45f0a6fab4b0fab6e77b039a37f9
gichani <thdrlcks97@gmail.com>
2024-08-30 13:59:41 +0900

    feat : 회원 전체 조회 api master, manager 권한만 조회 가능

--- src/main/java/com/sparta/bobdoduk/auth/controller/userController.java
+++ src/main/java/com/sparta/bobdoduk/auth/controller/userController.java
@@ -1,13 +1,28 @@
 package com.sparta.bobdoduk.auth.controller;
 
-import com.sparta.bobdoduk.auth.service.AuthService;
+import com.sparta.bobdoduk.auth.domain.User;
+import com.sparta.bobdoduk.auth.dto.UserResponseDto;
+import com.sparta.bobdoduk.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class userController {
 
+    private final UserService userService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('MASTER') or hasRole('MANAGER')")
+    public ResponseEntity<List<UserService.UserAllResponseDto>> getAllUsers() {
+
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
 }
