9088301636ba8e04388ac65cd5429ae305a52f90
kyungjinleelee <lv_jin@naver.com>
2024-09-01 17:31:43 +0900

    feat #20: 리뷰 및 평점 엔티티 클래스 작성

--- src/main/java/com/sparta/bobdoduk/review/domain/Report.java
+++ src/main/java/com/sparta/bobdoduk/review/domain/Report.java
@@ -1,4 +1,40 @@
 package com.sparta.bobdoduk.review.domain;
 
-public class Report {
+import com.sparta.bobdoduk.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "p_reports")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "report_id")
+    private UUID reportId;
+
+    @OneToOne
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @Column(name = "is_reported", nullable = false)
+    private Boolean isReported; // 신고 여부
+
+    @Column(name = "report_message", columnDefinition = "text")
+    private String reportMessage; // 신고 메시지
+
+    @Builder
+    public Report(Review review, Boolean isReported, String reportMessage) {
+        this.review = review;
+        this.isReported = isReported;
+        this.reportMessage = reportMessage;
+    }
+
 }
