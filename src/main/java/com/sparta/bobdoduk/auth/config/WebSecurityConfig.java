9e5089ed8f37c3068e5bca6e456bc2f8e30d04cc
lsw71311 <lsw71311@naver.com>
2024-09-03 04:12:09 +0900

    refactor #17: 공지사항 조회 권한 설정

--- src/main/java/com/sparta/bobdoduk/auth/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/bobdoduk/auth/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -67,6 +68,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/options/**").permitAll()
                         .requestMatchers("/api/products/**").permitAll()
                         .requestMatchers("/ai-content").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/notices").permitAll() // 공지사항 조회 접근 허용
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
