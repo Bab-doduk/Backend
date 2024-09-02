442e64c17f69a02478267a32ec03b855798928fe
lsw71311 <lsw71311@naver.com>
2024-09-02 16:03:28 +0900

    refactor #17: enumerated 설정, baseentity 처리

--- src/main/java/com/sparta/bobdoduk/orders/domain/Order.java
+++ src/main/java/com/sparta/bobdoduk/orders/domain/Order.java
@@ -1,5 +1,6 @@
 package com.sparta.bobdoduk.orders.domain;
 
+import com.sparta.bobdoduk.global.entity.BaseEntity;
 import com.sparta.bobdoduk.orders.dto.OrderReqDto;
 import com.sparta.bobdoduk.payment.domain.Payment;
 import jakarta.persistence.*;
@@ -13,10 +14,10 @@ import java.util.UUID;
 @Data
 @Builder
 @Getter @Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Table(name="p_orders")
-public class Order {
+public class Order extends BaseEntity {
 
     @Id
     private UUID orderId;
@@ -27,9 +28,11 @@ public class Order {
     @Column(name = "store_id")
     private UUID storeId;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "order_status")
     private OrderStatus orderStatus;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "order_type")
     private OrderType orderType;
 
