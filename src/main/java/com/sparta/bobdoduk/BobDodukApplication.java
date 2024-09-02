169b049f5b15e364ede1df2f1fc11ac617a3321b
gichani <thdrlcks97@gmail.com>
2024-09-02 13:17:03 +0900

    Revert "feat : auditorAware 적용"
    This reverts commit c4d9c4c48be1a752c7e3e7b19983a05e2e607652.

--- src/main/java/com/sparta/bobdoduk/BobDodukApplication.java
+++ src/main/java/com/sparta/bobdoduk/BobDodukApplication.java
@@ -2,8 +2,11 @@ package com.sparta.bobdoduk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BobDodukApplication {
 
     public static void main(String[] args) {
